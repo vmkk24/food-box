a48731b8911cec97ae9df22a2ddb8bdd53b78d18
nagashayanks <user1@LP-5CD9176M8P.HCLT.CORP.HCL.IN>
2020-02-05 19:29:32 +0530

    header component

--- src/app/service/url-config.ts
+++ src/app/service/url-config.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class UrlConfig {
     serverConfig = false;
-    private apiHost = 'http://10.117.189.175:8080/mealbox/';
+    private apiHost = 'http://10.117.189.181:8080/mealbox/';
     private apiMock = 'http://localhost:3000/';
     url = {};
 
